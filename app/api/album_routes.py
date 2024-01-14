46d01569d63da6d021e205b6bbbb2c0e06612869
Anthony Garcia <triplegdev@gmail.com>
2024-01-14 17:47:39 -0500

    initial commit

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from app.models import Album
+from flask_login import current_user, login_required
 
 album_routes = Blueprint('albums', __name__)
 
@@ -7,3 +8,11 @@ album_routes = Blueprint('albums', __name__)
 def get_albums():
     albums = Album.query.all()
     return {'albums': [album.to_dict() for album in albums]}
+
+@album_routes.route('/current')
+@login_required
+def get_user_likes():
+    print(current_user.albums)
+    return {
+        "user albums": [album.to_dict() for album in current_user.albums]
+    }
