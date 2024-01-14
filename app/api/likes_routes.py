ef235cb0a2dda96084d19852d89216883fa1857f
Jael Bueno <80376392+SejuSpeaks@users.noreply.github.com>
2024-01-14 17:31:21 -0500

    took print statement off

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -7,7 +7,6 @@ likes_router = Blueprint('likes', __name__)
 @likes_router.route('/current')
 @login_required
 def get_user_likes():
-    print(current_user.likes)
     return {
         "user likes": [like.album.to_dict() for like in current_user.likes]
     }
