58a01f7efa8dc7fd02a99a9cd4559105a7e35208
Jael <humanerror787@gmail.com>
2024-01-14 20:31:04 -0500

    add and remove album likes route

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
-from app.models import Album
+from flask_login import current_user
+from app.models import Album, Like, db
 
 album_routes = Blueprint('albums', __name__)
 
@@ -22,3 +23,33 @@ def album_likes(id):
     return {
         f"{album.title} likes": [{"user_id" : like.user.id} for like in album_likes]
     }
+
+
+@album_routes.route('/<int:id>/likes', methods=['POST','DELETE'])
+@album_routes.errorhandler(404)
+def like_album(id):
+    album = Album.query.get(id)
+
+    query_for_like = Like.query.filter((Like.user_id == current_user.id) & (Like.album_id == id)).all()
+
+    #LIKE IS ALREADY PRESENT SO WE DELETE IT
+    if (query_for_like):
+        delete_like = Like.query.filter((Like.user_id == current_user.id) & (Like.album_id == id)).delete()
+        db.session.commit()
+        return {"DELETE": "Album like removed"}
+
+    #IF ALBUM IS NOT FOUND
+    if(not album):
+        error = {"Error": "Invalid album id"}
+        return error, 404
+
+    #ADDING LIKE
+    like = Like(
+        album_id=album.id,
+        user_id=current_user.id
+        )
+
+    db.session.add(like)
+    db.session.commit()
+
+    return {"Success":"Album added to Likes"}
