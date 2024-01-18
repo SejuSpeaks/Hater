36962ea7423a5514c77a902607980052e27fd49d
h-guertler <guertler99@gmail.com>
2024-01-17 22:16:55 -0500

    ready to test create a review

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app.models import Album, Like, User, Review, db
 from flask_login import current_user, login_required
 from datetime import datetime
 from sqlalchemy import or_, func
+from app.forms import ReviewForm
+# from app.api import validation_errors_to_error_messages
 
 album_routes = Blueprint('albums', __name__)
 
@@ -92,3 +94,64 @@ def like_album(id):
     db.session.commit()
 
     return {"Success":"Album added to Likes"}
+
+@album_routes.route('/<int:id>/reviews', methods=['POST'])
+# Exits with status 404 if no album with the ID in the URL exists
+@album_routes.errorhandler(404)
+@login_required
+def add_album_review(id):
+    current_user_id = current_user.id
+    album_id = id
+
+    # Searches to see whether there is already a review of the album by the user
+    existing_review = db.session.query(Review) \
+        .filter(Review.user_id == current_user_id, Review.album_id == album_id) \
+        .first()
+
+    # If it exists, exits with status 403
+    if existing_review:
+        error = {"Error": "You have already reviewed this album."}
+        return error, 403
+
+    # Collects review data from user:
+    else:
+        review_form = ReviewForm()
+        # create a new review
+        review_form['csrf_token'].data = request.cookies['csrf_token']
+
+        if review_form.validate_on_submit():
+            new_review_text = review_form.review_text.data
+            new_review_rating = review_form.review_rating.data
+
+            # Creates a new review and adds it to the database
+            new_review = Review(
+                user_id = current_user_id,
+                album_id = album_id,
+                review_text = new_review_text,
+                rating = new_review_rating
+            )
+
+            db.session.add(new_review)
+            db.session.commit()
+
+            # Finds the newly created review to confirm sucess of operation
+            created_review = db.session.query(Review) \
+                .filter(Review.user_id == current_user_id, Review.album_id == album_id) \
+                .first()
+
+            return created_review.to_dict()
+
+        # Returns all error messages from the review form
+        else:
+            return {'errors': review_form.errors}, 400
+
+@album_routes.route('/<int:id>/reviews', methods=['GET'])
+# Exits with status 404 if no album with the ID in the URL exists
+@album_routes.errorhandler(404)
+def get_album_reviews(id):
+    # Queries the reviews by joining the Users table and Albums table
+    reviews_users_query = db.session.query(Review, User) \
+        .outerjoin(User, Review.user_id == User.id) \
+        .filter(Review.album_id == id).all()
+
+    return { "Reviews": reviews_users_query }
--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask_login import current_user, login_required
+from app.models import Review, db
 
 review_router = Blueprint('reviews', __name__)
 
@@ -9,3 +10,48 @@ def get_user_reviews():
     return {
         "user reviewed albums": [review.album.to_dict() for review in current_user.reviews]
     }
+
+
+@review_router.route('/<int:id>', methods=['DELETE'])
+# Exits with status 404 if no review with the ID in the URL exists
+@review_router.errorhandler(404)
+@login_required
+def delete_review(id):
+    # Selects the review to be deleted
+    doomed_review = Review.query.get(id)
+
+    # Sends an error response if the review was not created by the current user:
+    if not Review.user_id == current_user.id:
+        error = {"Error": "You are not authorized to delete this review."}
+        return error, 401
+
+    # Deletes the review and sends a success message upon completion
+    else:
+        db.session.delete(doomed_review)
+        db.session.commit()
+        success_msg = { "Success": "Review has been removed" }
+        return success_msg, 200
+
+@review_router.route('/<int:id>', methods=['PUT'])
+# Exits with status 404 if no review with the ID in the URL exists
+@review_router.errorhandler(404)
+@login_required
+def edit_review(id):
+    # Selects the review to be edited
+    review = Review.query.get(id)
+
+    # Returns an error if the review was not created by the current user
+    if not review.user_id == current_user.id:
+        error = {"Error": "You are not authorized to edit this review."}
+        return error, 401
+
+    # Create a form to edit the review
+        # this should be pre-populated with the info of the review found above
+
+    # If the form passes validation:
+        # Edit the selected review to contain the newly submitted info + new updated_at date
+        # Also add it to the session
+        # Return updated review with id, userId, albumId, reviewtext, rating, createdAt, and updatedAt
+
+    # If it does not pass validation:
+        # Return all errors generated with status 400, but don't clear error-free form fields
