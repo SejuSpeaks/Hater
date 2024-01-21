713035a56cba92c9ef0d533c5eaea890e1f7a836
h-guertler <guertler99@gmail.com>
2024-01-21 09:24:55 -0500

    EOD Friday

---
+++ react-app/src/store/review.js
@@ -0,0 +1,44 @@
+// constants
+const ADD_REVIEW = "session/ADD_REVIEW";
+
+const addReview = (review) => ({
+	type: ADD_REVIEW,
+	payload: review,
+});
+
+const initialState = { reviews: null };
+
+export const addAReview = (albumId, reviewText, rating) => async (dispatch) => {
+	const response = await fetch(`/api/albums/${albumId}/reviews`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			reviewText,
+            rating
+		}),
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(addReview(data));
+		return null;
+	} else if (response.status < 500) {
+		const data = await response.json();
+		if (data.errors) {
+			return data.errors;
+		}
+	} else {
+		return ["An error occurred. Please try again."];
+	}
+};
+
+export default function reducer(state = initialState, action) {
+	switch (action.type) {
+		case ADD_REVIEW:
+			return { newReview: action.payload };
+		default:
+			return state;
+	}
+}
