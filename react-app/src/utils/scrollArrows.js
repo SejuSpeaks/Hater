e79c724b3ab9a4e6a1ac66525ab362867ad9f5d1
Jael <humanerror787@gmail.com>
2024-03-28 21:32:52 -0400

    no consolelog

--- react-app/src/utils/scrollArrows.js
+++ react-app/src/utils/scrollArrows.js
@@ -1,6 +1,5 @@
 
 export const isOverflown = (parentContainer, childContainer, setState) => {
-    console.log(childContainer.length * 180, parentContainer.offsetWidth)
     if (parentContainer.offsetWidth < childContainer.length * 180 && parentContainer.offsetWidth >= 430) {
         setState(true);
     }
